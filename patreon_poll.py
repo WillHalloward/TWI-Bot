5db4639bebd51dc6b64cdd6b73adb6f5921d0c6c
Will_Halloward <willhalloward@gmail.com>
2019-06-24 10:49:45 +0300

    Reworked x to be less stupid. Added TODO

--- patreon_poll.py
+++ patreon_poll.py
@@ -8,7 +8,6 @@ import secrets
 
 
 async def p_poll(ctx):
-    x = 0
     url = "https://www.patreon.com/api/polls/218745"
     page = requests.get(url, cookies=secrets.cookies)
     json_data = json.loads(page.text)
@@ -21,10 +20,7 @@ async def p_poll(ctx):
     n_options = json_data['data']['relationships']['choices']['data']
     embed = discord.Embed(title="Poll", color=discord.Color(0x3cd63d), description="**{}**".format(title))
     embed.set_footer(text="Poll started at {} and closes at {}".format(open_at_converted, closes_at_converted))
-
-    for numer in n_options:
-        x += 1
-    print(x)
+    x = len(n_options)
     for i in range(0, x):
         print(option[i]['attributes']['text_content'])
         print(option[i]['attributes']['num_responses'])
@@ -32,3 +28,8 @@ async def p_poll(ctx):
                         value="{}".format(option[i]['attributes']['num_responses']), inline=False)
     # for options in option:
     await ctx.send(embed=embed)
+# TODO Get latest poll automatically
+# TODO Make poll automagically update every x min.
+# TODO make bot pin poll.
+# TODO make 2 separate commands, !setpoll and poll
+# TODO make footer time in local time.
